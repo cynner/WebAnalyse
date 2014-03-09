6cbf458ba6ccd7d5bc01aae1062c6db1aef8b816
wiwat <wiwat@malang>
2014-03-09 22:01:05 +0700

    Add ExtractLink and SCCDynamic  #3
    *Change initial file name input/output in common sense [fixed]

--- src/Utils/ExtractLink.java
+++ src/Utils/ExtractLink.java
@@ -223,7 +223,7 @@ public class ExtractLink {
         String FNDirIn = args.length > 0 ? args[0] : "data/testarc";
         String FNOut = args.length > 1 ? args[1] : "data/graph.result";
         
-        ExtractLink el = new ExtractLink(new File(FNOut + ".webpage"),new File(FNOut + ".host"));
+        ExtractLink el = new ExtractLink(new File(FNOut + ".host"),new File(FNOut + ".webpage"));
         try {
             el.runExtractLinkDir(new File(FNDirIn));
         } catch (IOException ex) {
