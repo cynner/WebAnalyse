24b7d48c114e0ee3efabbc24ae92866983e70625
Malang <Malang@Malang-PC>
2014-03-24 17:19:34 +0700

    Add debug for link extraction
    *Add debug for link extraction

--- src/Utils/ExtractLink.java
+++ src/Utils/ExtractLink.java
@@ -179,6 +179,7 @@ public class ExtractLink {
                         mi.increment();
                     }
                 } catch (Exception ex) {
+                    System.err.println("At : " + src.UniqURL);
                     Logger.getLogger(ExtractLink.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
